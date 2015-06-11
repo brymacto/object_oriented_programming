8db5296977bce00402351d6ad62a4f38e747ae29
Bryan <brymac@gmail.com>
2015-06-11 16:58:09 -0400

    Move and rotate working.  Read input in progress

--- rover.rb
+++ rover.rb
@@ -1,39 +1,61 @@
+$plateau_width = 10
+$plateau_height = 10
 class Rover
+
   attr_accessor :x, :y, :direction
-  def initialize(x, y, direction)
+  def initialize(x, y, direction = "S")
     @x = x
     @y = y
     @direction_options = ["N", "E", "S", "W"]
     @direction = direction
   end
 
-  def read_instruction(instruction)
-      if instruction.is_a?(Integer) == true
+  def to_s
+    "Position: (#{@x}, #{@y}), Direction: #{@direction}"
+  end
+  def read_input
 
-
-      else
-        # case instruction
-
-        # end
-
-      end
   end
 
-  def move(distance)
+  def read_instruction(instruction)
+    case instruction
+    when "M"
+      move
+    when "L", "R"
+      turn(instruction)
+    else
+      puts "Your instruction #{instruction} is not valid.  Please enter L, R, or M."
+    end
+  end
 
-
-
+  def move
+    case @direction
+    when "N"
+      @y += 1 unless y == $plateau_height
+    when "E"
+      @x += 1 unless x == $plateau_width
+    when "S"
+      @y -= 1 unless y == 0
+    when "W"
+      @x -= 1 unless x == 0
+    end
   end
 
   def turn(turn_dir)
     case turn_dir
     when "L"
-      @direction = @direction_options[(@direction_options.index(@direction) - 1)]
-
+        if @direction != "N"
+          @direction = @direction_options[(@direction_options.index(@direction) - 1)]
+        else
+          @direction = "W"
+        end
+    when "R"
+        if @direction != "W"
+          @direction = @direction_options[(@direction_options.index(@direction) + 1)]
+        else
+          @direction = "N"
+        end
     end
-
-
-
   end
 
   def test
@@ -43,10 +65,24 @@ class Rover
 
 end
 
+rover = Rover.new(0,0)
 
-bob = Rover.new(0,0,"N")
-5.times do
-bob.turn("L")
-puts bob.direction
-
+#TESTING
+4.times do
+  rover.turn("L")
+  20.times do
+    rover.move
+    puts rover
+  end
 end
+
+
+
+
+
+
+
+# inputstring.each_character {|x|  }
+#
+# array.rotate
+# rotates array to help you figure out what direction it's facing
